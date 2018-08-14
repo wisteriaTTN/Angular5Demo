f8027729946e96e0216bd601595dd574cca7a208
Thien Nhu <tranthiennhu1904@gmail.com>
2018-08-14 23:20:36 +0700

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular5Demo';
+  faListAlt = faListAlt;
+  faShoppingCart = faShoppingCart;
 }
